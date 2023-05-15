0a52635001ff9c88aaa72bc9cf4803b3084769d4
acharwekar <akshay.charwekar@agoda.com>
2023-05-15 22:36:48 +0700

    Added header section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
+import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: 'IMDB',
-  description: 'We are the Internet Movie Database',
+    title: 'IMDB',
+    description: 'We are the Internet Movie Database',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Header/>
+                {children}
+            </body>
+        </html>
+    )
 }
---
+++ components/MenuItem.tsx
@@ -0,0 +1,22 @@
+import {IconType} from "react-icons";
+import Link from "next/link";
+
+interface MenuItemProps {
+    title: string;
+    address: string;
+    icon: IconType;
+}
+
+const MenuItem = (props: MenuItemProps) => {
+    const {title, address, icon: Icon} = props;
+    return (
+        <div>
+            <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600">
+                <Icon className="text-2xl sm:hidden mx-4"/>
+                <p className="hidden sm:inline my-2 text-sm">{title}</p>
+            </Link>
+        </div>
+    )
+};
+
+export default MenuItem;
