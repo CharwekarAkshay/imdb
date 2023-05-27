56e455fd881c9accfadd41af50316976f940609c
acharwekar <akshay.charwekar@agoda.com>
2023-05-27 13:02:28 +0700

    Added about section

---
+++ app/about/page.tsx
@@ -0,0 +1,30 @@
+const About = () => {
+    return (
+        <div className="max-w-6xl mx-auto  space-y-4">
+            <h1 className="text-2xl font-medium text-amber-600">About</h1>
+            <p>Welcome to our IMDb Clone website, the ultimate destination for movie lovers, entertainment enthusiasts,
+                and industry professionals alike! Our platform offers an extensive and regularly-updated database of
+                films, TV shows, web series, documentaries, and more, along with the latest news and updates about your
+                favorite actors, directors, producers, screenwriters, and other creative forces in the industry.</p>
+
+            <p>At our IMDb Clone website, you can explore a wide range of genres, from action and adventure to indie
+                dramas and foreign-language masterpieces, discovering hidden gems and timeless classics while staying
+                up-to-date with the latest releases and box office hits. We take pride in providing accurate
+                information, unbiased ratings, and insightful reviews that are submitted by our dedicated and passionate
+                community of cinephiles, TV show aficionados, and entertainment connoisseurs from across the globe.</p>
+
+            <p>Key features of our website include: Comprehensive and sortable directories for movies, TV shows, web
+                series, documentaries, and more, with detailed information on cast, crew, release dates, plot synopsis,
+                and production details; Crowd-sourced ratings and reviews; Trending entertainment news, celebrity
+                interviews, and exclusive behind-the-scenes insights; Accurate box office figures, industry statistics,
+                and awards information; A powerful search function with advanced filtering options, allowing you to
+                easily find and discover content based on your preferences; User-friendly and customizable watchlists,
+                lists, ratings, and personalized recommendations tailored to your viewing habits, interests, and tastes.
+                Join our thriving international community today and immerse yourself in a world of cinematic wonders.
+                Our IMDb Clone website is your one-stop destination for all things movies, TV, and beyond, so sit back,
+                relax, and let the magic of entertainment envelop you!</p>
+        </div>
+    );
+};
+
+export default About;
